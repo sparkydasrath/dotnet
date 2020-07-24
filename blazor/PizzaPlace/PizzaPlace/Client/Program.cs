d3c9c650b9468b6ef23f94bb4fee79a94f7be486
sparky dasrath <sdasrath@gmail.com>
2020-07-23 23:09:46 -0400

    Added and injected services for placing order and fetching menu

--- blazor/PizzaPlace/PizzaPlace/Client/Program.cs
+++ blazor/PizzaPlace/PizzaPlace/Client/Program.cs
@@ -1,12 +1,9 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using PizzaPlace.Shared;
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace PizzaPlace.Client
 {
@@ -18,7 +15,8 @@ namespace PizzaPlace.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddTransient<IMenuService, HardCodedMenuService>();
+            builder.Services.AddTransient<IOrderService, ConsoleOrderService>();
             await builder.Build().RunAsync();
         }
     }
